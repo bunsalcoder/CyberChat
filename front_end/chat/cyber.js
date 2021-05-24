1cf2affe102980e2c138dba0b9bde64581a915ff
Bunsal.Hul <bunsal.hul@student.passerellesnumeriques.org>
2021-05-24 14:49:39 +0700

    send and receive testing

--- front_end/chat/cyber.js
+++ front_end/chat/cyber.js
@@ -18,9 +18,9 @@ function displayMessages(messages){
 
     let currentTime = new Date();
 
-    // if (messageTitle !== null){
-    //     messageTitle.remove();
-    // };
+    if (messageTitle !== null){
+        messageTitle.remove();
+    };
 
     const newMessageTitle = document.createElement("div");
     newMessageTitle.className = 'message-title';
@@ -59,7 +59,7 @@ function displayMessages(messages){
             otherMessage.appendChild(newMessageTitle);
         };    
     };
-    message.value = '';
+    // message.value = '';
 };
 
 //_____________________send message______________________//
